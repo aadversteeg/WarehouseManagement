30acd2fd13f31aaed96df33f5bcadfb37f59ffe2
Aad Versteeg <aad@versteeg-its.nl>
2020-11-09 07:18:37 +0100

    Added value objects for ids of batcches and locations

---
+++ src/Core.Domain/BatchId.cs
@@ -0,0 +1,30 @@
+﻿using Core.Extensions;
+using System.Collections.Generic;
+
+namespace Core.Domain
+{
+    public class BatchId : ValueObject
+    {
+        private int _value;
+
+        private BatchId(int value)
+        {
+            _value = value;
+        }
+
+        public static Result<BatchId> Create(int value)
+        {
+            return Result.Success(new BatchId(value));
+        }
+
+        protected override IEnumerable<object> GetAtomicValues()
+        {
+            yield return _value;
+        }
+
+        public override string ToString()
+        {
+            return _value.ToString();
+        }
+    }
+}
---
+++ src/Core.Domain/LocationId.cs
@@ -0,0 +1,30 @@
+﻿using Core.Extensions;
+using System.Collections.Generic;
+
+namespace Core.Domain
+{
+    public class LocationId : ValueObject
+    {
+        private int _value;
+
+        private LocationId(int value)
+        {
+            _value = value;
+        }
+
+        public static Result<LocationId> Create(int value)
+        {
+            return Result.Success(new LocationId(value));
+        }
+
+        protected override IEnumerable<object> GetAtomicValues()
+        {
+            yield return _value;
+        }
+
+        public override string ToString()
+        {
+            return _value.ToString();
+        }
+    }
+}
